84ab6ec0cadbaba5896d2ce48a5204a247fda09e
shashi <47247709+xshashi@users.noreply.github.com>
2021-07-03 02:51:41 +0300

    Add files via upload

---
+++ arms.sql
@@ -0,0 +1,43 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 02, 2021 at 06:08 PM
+-- Server version: 10.4.19-MariaDB
+-- PHP Version: 8.0.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `robot arm`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `arms`
+--
+
+CREATE TABLE `arms` (
+  `slider_one` int(3) NOT NULL,
+  `slider_two` int(3) NOT NULL,
+  `slider_three` int(3) NOT NULL,
+  `slider_four` int(3) NOT NULL,
+  `slider_five` int(3) NOT NULL,
+  `slider_six` int(3) NOT NULL,
+  `ON/OFF` boolean(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
